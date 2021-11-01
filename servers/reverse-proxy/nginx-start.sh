4e34acd7ad72906f2673c8a3ab5008a7bfccd8ab
Benedikt Schmidt <benediktibk@gmail.com>
2021-11-01 11:00:10 +0100

    gracefully stop reverse proxy

--- servers/reverse-proxy/nginx-start.sh
+++ servers/reverse-proxy/nginx-start.sh
@@ -3,4 +3,4 @@ set -eu
 
 envsubst '${WEBFQDN} ${TARGETCORONA} ${TARGETME} ${TARGETDOWNLOADS}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
-nginx -g 'daemon off;'
+exec nginx -g 'daemon off;'
