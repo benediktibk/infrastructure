8f86a2c09efc3d2ea796c5cdb6076f7b540462b7
Benedikt Schmidt <benediktibk@gmail.com>
2021-11-02 18:42:55 +0100

    ensure that the alternate name is set as well in the certificates

--- scripts/create-certificate.sh
+++ scripts/create-certificate.sh
@@ -9,11 +9,15 @@ if [ ! -n "$2" ]; then echo "common name is not specified"; exit 1; fi
 KEYFILE=build/secrets/ca/$1.key
 CSRFILE=build/secrets/ca/$1.csr
 CERTFILE=build/secrets/ca/$1.crt
+CERTTEMPFILE=build/secrets/ca/$1_temp.crt
 ROOTCACERTFILE=build/secrets/ca/root_ca.crt
 ROOTCAKEYFILE=build/secrets/ca/root_ca.key
 
+cp scripts/v3.ext.in build/v3.ext
+sed -i "s/##ALTERNATIVENAME##/$2/g" build/v3.ext
+
 openssl genrsa -out $KEYFILE 4096
-openssl req -new -sha512 -key $KEYFILE -subj "/C=AT/O=benediktschmidt.at/CN=$2" -out $CSRFILE
-openssl x509 -req -in $CSRFILE -CA $ROOTCACERTFILE -CAkey $ROOTCAKEYFILE -CAcreateserial -out ${CERTFILE}_temp -days 1825 -sha512
-cat ${CERTFILE}_temp $ROOTCACERTFILE > $CERTFILE
-rm ${CERTFILE}_temp
+openssl req -new -sha512 -key $KEYFILE -subj "/C=AT/O=benediktschmidt.at/CN=$2" -addext "subjectAltName = DNS:$2" -out $CSRFILE
+openssl x509 -req -in $CSRFILE -CA $ROOTCACERTFILE -CAkey $ROOTCAKEYFILE -CAcreateserial -out $CERTTEMPFILE -days 1825 -sha512 -extfile build/v3.ext
+cat $CERTTEMPFILE $ROOTCACERTFILE > $CERTFILE
+rm $CERTTEMPFILE
