16a701e98d25d1d8b14bf5705f4b3e36f9009971
Benedikt Schmidt <benediktibk@gmail.com>
2021-11-02 15:42:41 +0100

    add script to create custom ca signed certificates

---
+++ scripts/create-certificate.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo $1
+echo $2
+
+if [ ! -n "$1" ]; then echo "certificate name is not specified"; exit 1; fi
+if [ ! -n "$2" ]; then echo "common name is not specified"; exit 1; fi
+
+KEYFILE=build/secrets/ca/$1.key
+CSRFILE=build/secrets/ca/$1.csr
+CERTFILE=build/secrets/ca/$1.crt
+ROOTCACERTFILE=build/secrets/ca/root_ca.crt
+ROOTCAKEYFILE=build/secrets/ca/root_ca.key
+
+openssl genrsa -out $KEYFILE 4096
+openssl req -new -sha512 -key $KEYFILE -subj "/C=AT/O=benediktschmidt.at/CN=$2" -out $CSRFILE
+openssl x509 -req -in $CSRFILE -CA $ROOTCACERTFILE -CAkey $ROOTCAKEYFILE -CAcreateserial -out $CERTFILE -days 1825 -sha512
