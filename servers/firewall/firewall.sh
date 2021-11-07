e252080f53066bc74dafe17e111d36ac90f9a782
Benedikt Schmidt <benediktibk@gmail.com>
2021-11-07 15:50:20 +0100

    configure basic rules for firewall

--- servers/firewall/firewall.sh
+++ servers/firewall/firewall.sh
@@ -6,12 +6,16 @@ trap "echo 'got signal to stop, exiting'; exit 0;" SIGHUP SIGINT SIGTERM
 echo "host IP: $HOSTIP"
 echo "host VPN IP: $HOSTVPNIP"
 
+nft delete chain ip filter INPUT
 nft 'add chain ip filter INPUT { type filter hook input priority 0; policy accept; }'
 nft add rule filter INPUT iifname "lo" accept
-nft add rule filter INPUT ip daddr $HOSTVPNIP tcp dport 22 counter
-nft add rule filter INPUT ip daddr $HOSTIP tcp dport 1194 counter
-
-#nft 'add chain ip filter INPUT { type filter hook input priority 0; policy drop; }'
+nft add rule filter INPUT ct state established accept
+nft add rule filter INPUT ip daddr $HOSTVPNIP tcp dport 22 accept
+nft add rule filter INPUT ip daddr $HOSTIP tcp dport 1194 accept
+nft add rule filter INPUT ip daddr $HOSTIP tcp dport 80 accept
+nft add rule filter INPUT ip daddr $HOSTIP tcp dport 443 accept
+nft add rule filter INPUT log prefix "nft.ip.filter.input.drop "
+nft 'add chain ip filter INPUT { type filter hook input priority 0; policy drop; }'
 
 while :
 do
