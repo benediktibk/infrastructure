6d04d957c20c4b909cc044cf1adc410aed1d33f4
Benedikt Schmidt <benediktibk@gmail.com>
2021-11-22 22:03:31 +0100

    fix valheim server name and password

--- servers/valheim/start_server.sh
+++ servers/valheim/start_server.sh
@@ -3,9 +3,11 @@
 echo "install trap for signals"
 trap "echo 'got signal to stop, exiting'; exit 0;" SIGHUP SIGINT SIGTERM
 
+echo "starting valheim server $SERVER_NAME with password $SERVER_PASSWORD"
+
 export LD_LIBRARY_PATH=./linux64:$LD_LIBRARY_PATH
 export SteamAppId=892970
-./valheim_server.x86_64 -name $VALHEIM_SERVER_NAME -port 2456 -world "Dedicated" -password $VALHEIM_SERVER_PASSWORD -public 1 -savedir /srv/valheim &
+./valheim_server.x86_64 -name $SERVER_NAME -port 2456 -world "Dedicated" -password $SERVER_PASSWORD -public 1 -savedir /srv/valheim &
 wait $!
 
 echo "valheim process stopped unexpected"
