9f01bad6a3fdedc1f89b5d88f0124761c45bd993
Benedikt Schmidt <benediktibk@gmail.com>
2021-11-22 22:50:51 +0100

    stop valheim server gracefully

--- servers/valheim/start_server.sh
+++ servers/valheim/start_server.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-echo "install trap for signals"
-trap "echo 'got signal to stop, exiting'; exit 0;" SIGHUP SIGINT SIGTERM
-
 echo "starting valheim server $SERVER_NAME with password $SERVER_PASSWORD"
 
 export LD_LIBRARY_PATH=./linux64:$LD_LIBRARY_PATH
 export SteamAppId=892970
 ./valheim_server.x86_64 -name $SERVER_NAME -port 2456 -world "Dedicated" -password $SERVER_PASSWORD -public 1 -savedir /srv/valheim &
-wait $!
+PROCESSID=$!
+
+echo "install trap for signals"
+trap "echo 'got signal to stop, sending SIGINT to $PROCESSID'; kill -s SIGINT $PROCESSID; echo 'waiting for $PROCESSID to finish'; wait $PROCESSID; echo 'everything stopped gracefully'; exit 0;" SIGHUP SIGINT SIGTERM
+
+wait $PROCESSID
 
 echo "valheim process stopped unexpected"
 exit 1
