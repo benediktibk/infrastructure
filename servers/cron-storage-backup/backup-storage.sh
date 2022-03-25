3ea9d93f6fc9b107486a0deb8304c3a6cbbeef8c
Benedikt Schmidt <benediktibk@gmail.com>
2022-03-25 15:56:04 +0100

    exclude more from the storage backup

--- servers/cron-storage-backup/backup-storage.sh
+++ servers/cron-storage-backup/backup-storage.sh
@@ -4,5 +4,5 @@ umount /mnt/storage1 > /dev/null 2>&1
 
 set -e
 mount.cifs -o username=system-cron-storage,domain=benediktschmidt.at,password=$DOMAINPASSWORD,rw //192.168.42.4/data /mnt/storage1
-rsync --recursive --delete-during --exclude Multimedia/Filme /mnt/storage1/ /mnt/storagebackup
+rsync --recursive --delete-during --exclude Multimedia/Filme --exclude Multimedia/Serien --exclude Software /mnt/storage1/ /mnt/storagebackup
 umount /mnt/storage1
