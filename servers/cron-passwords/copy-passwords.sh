085c9186b75e31da8714ba6996ca1f45d17b3523
Benedikt Schmidt <benediktibk@gmail.com>
2022-03-25 12:25:04 +0100

    specify SMB protocol version for cron-passwords

--- servers/cron-passwords/copy-passwords.sh
+++ servers/cron-passwords/copy-passwords.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-smbclient -U system-cron-password@benediktschmidt.at%$DOMAINPASSWORD //192.168.42.4/data --directory User/benedikt/Documents/Passwoerter -c 'get passwords.kdbx'
-smbclient -N //192.168.39.8/data -c 'put passwords.kdbx'
+smbclient --max-protocol=SMB2 -U system-cron-password@benediktschmidt.at%$DOMAINPASSWORD //192.168.42.4/data --directory User/benedikt/Documents/Passwoerter -c 'get passwords.kdbx'
+smbclient --max-protocol=SMB2 -N //192.168.39.8/data -c 'put passwords.kdbx'
