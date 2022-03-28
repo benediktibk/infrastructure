6dc25f8a40bf1845f4c13a33f5f354e7f7b59b8d
Benedikt Schmidt <benediktibk@gmail.com>
2022-03-28 15:58:24 +0200

    allow zabbix agent on server-1

--- servers/firewall/firewall.sh
+++ servers/firewall/firewall.sh
@@ -79,6 +79,8 @@ nft add rule filter INPUT ip daddr $HOSTIP tcp dport 443 counter accept
 nft add rule filter INPUT ip daddr $HOSTIP tcp dport 2456-2458 counter accept
 nft add rule filter INPUT ip daddr $HOSTIP udp dport 2456-2458 counter accept
 nft add rule filter INPUT ip daddr $HOSTIP udp dport 22023 counter accept
+echo "    allow specific services from internal"
+nft add rule filter INPUT ip daddr 192.168.39.1 tcp dport 10050 counter accept
 echo "    allow ICMP requests"
 nft add rule filter INPUT icmp type echo-request counter accept
 echo "    log dropped packets"
