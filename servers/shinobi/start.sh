84984ea89cb625d9da33399c8e5663668a01703c
Benedikt Schmidt <benediktibk@gmail.com>
2022-10-09 14:58:52 +0200

    fix startup behaviour of shinobi

--- servers/shinobi/start.sh
+++ servers/shinobi/start.sh
@@ -7,8 +7,7 @@ cp /super.json.template /opt/shinobi/super.json
 sed -i "s/##DBPASSWORD##/$MARIADB_PASSWORD/g" /opt/shinobi/conf.json
 sed -i "s/##SHINOBIADMINPASSWORD##/$SHINOBIADMINPASSWORD/g" /opt/shinobi/super.json
 
-echo "execute shinobi in foreground"
+echo "execute shinobi in foreground slightly delayed"
 cd /opt/shinobi/
-pm2 start camera.js
 pm2 start cron.js
-exec pm2 logs
+exec pm2-runtime camera.js
